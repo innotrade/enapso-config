d96da1f424f88656b793fd512fc1830122d09047
Ashesh Goplani <ashesh.goplani96@gmail.com>
2020-10-28 16:01:21 +0500

    new change

--- config/enapso-resources_ashesh_pc1.js
+++ config/enapso-resources_ashesh_pc1.js
@@ -1,9 +1,19 @@
-const { DepartmentConfig } = require('./department_config');
+// in this file only machine specific settings should be configured!
 
-class ResourceManagerAshesh extends DepartmentConfig {
+const { EnapsoResourcesConfig } = require('./enapso_resources');
+
+class EnapsoResourcesAsheshPc1Config extends EnapsoResourcesConfig {
     constructor(data) {
         super(data);
+
+        Object.assign(this.backend, {
+            baseUrl: 'http://localhost:5000'
+        });
+
+        this.classes = ['http://ont.enapso.com/foundation#Resource'];
+        this.path = '';
         this.baseURL = 'http://localhost:5000';
+
         this.user = {
             Email: 'ash@gmail.com',
             Password: 'secure'
@@ -12,8 +22,8 @@ class ResourceManagerAshesh extends DepartmentConfig {
         this.ontology = {
             path: '../ontology/EnapsoFoundation.owl',
             format: 'application/rdf+xml',
-            baseIRI: 'http://ont.enapso.com/auth#',
-            context: 'http://ont.enapso.com/auth',
+            baseIRI: 'http://ont.enapso.com/foundation#',
+            context: 'http://ont.enapso.com/foundation',
             cls: 'http://ont.enapso.com/foundation#Resource',
             joins: [
                 {
@@ -81,15 +91,55 @@ class ResourceManagerAshesh extends DepartmentConfig {
                         }
                     ]
                 }
+            ],
+            iri: 'http://ont.enapso.com/foundation#00a5e37f_3452_4b48',
+            createRecord: {
+                iri: 'http://ont.enapso.com/foundation#00a5e37f_3452_4b48',
+                name: 'New Test Company',
+                uuid: '12345tyuiigfds',
+                dataId: 'hgfdsdddddd',
+                hash: 'Tqwerhvh',
+                rights: 'rwx',
+                code: 'function(option){console.log(option)}',
+                user: 'jnhgtresss',
+                hasCapabilities:
+                    'enf:Capability_00a5e37f_3452_4b48_8a0a_3089dc41ef47',
+                hasAttributes:
+                    'http://ont.enapso.com/foundation#Attribute_5ed0a3d9_a801_4c4b_a072_578090f60353',
+                hasBehavior:
+                    'http://ont.enapso.com/foundation#Behavior_03e35a1d_5dd2_44fd_a596_908a1474dec8'
+            },
+            updateRecord: {
+                iri: 'http://ont.enapso.com/foundation#00a5e37f_3452_4b48',
+                name: 'New Test Company',
+                uuid: '12345tyuiigfds',
+                dataId: 'hgfdsdddddd',
+                hash: 'Tqwerhvh',
+                rights: 'rwx',
+                code: 'function(option){console.log(option)}',
+                user: 'jnhgtresss',
+                hasCapabilities:
+                    'enf:Capability_00a5e37f_3452_4b48_8a0a_3089dc41ef47',
+                hasAttributes:
+                    'http://ont.enapso.com/foundation#Attribute_5ed0a3d9_a801_4c4b_a072_578090f60353',
+                hasBehavior:
+                    'http://ont.enapso.com/foundation#Behavior_03e35a1d_5dd2_44fd_a596_908a1474dec8'
+            },
+            filter: [
+                {
+                    key: '$sparql',
+                    value:
+                        'regEx(str(?ind), "http://ont.enapso.com/foundation#Resource_124ccf70_38eb_4ca0_9f7e_380c65a5a36e", "i")'
+                }
             ]
         };
-        this.classes = ['http://ont.enapso.com/foundation#Resource'];
-        this.path = '';
-        this.baseURL = 'http://localhost:5000';
+    }
+    getGlobalTarget() {
+        return '';
     }
 }
 
 module.exports = {
-    ResourceManagerAshesh,
-    config: ResourceManagerAshesh
+    EnapsoResourcesAsheshPc1Config,
+    config: EnapsoResourcesAsheshPc1Config
 };
