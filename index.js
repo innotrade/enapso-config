6e430a088f16f0f7e780f54103a5a373c0481ca9
Alexander Schulze <aschulze@innotrade.com>
2020-11-14 17:06:41 +0100

    Fixing module config hierarchy issue.

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ let encfg = {
     EnapsoConfig,
     RootConfig
 };
-global.encfg = encfg;
+
+// if no global object set yet, define it
+if (!global.encfg) {
+    global.encfg = encfg;
+    encfg.config = {};
+    encfg.notify = EnapsoConfig.notify;
+    encfg.MODE_DEV = EnapsoConfig.MODE_DEV;
+    encfg.MODE_PROD = EnapsoConfig.MODE_PROD;
+    encfg.MODE_STAGE = EnapsoConfig.MODE_STAGE;
+    encfg.MODE_DEFAULT = EnapsoConfig.MODE_DEFAULT;
+}
 
 // load the configuration from the user, application or machine specific config file
 const path = process.env.ENAPSO_CONFIG_FILE.replace(
@@ -28,19 +38,27 @@ const config = new configClass.config();
 process.env.ENAPSO_CONFIG_MODE = process.env.ENAPSO_CONFIG_MODE || 'default';
 config.setMode(process.env.ENAPSO_CONFIG_MODE);
 
-encfg = config;
-encfg.EnapsoConfig = EnapsoConfig;
-encfg.RootConfig = RootConfig;
 encfg.config = config;
 encfg.packages = config.packages;
-encfg.notify = config.notify;
-encfg.MODE_DEV = EnapsoConfig.MODE_DEV;
-encfg.MODE_PROD = EnapsoConfig.MODE_PROD;
-encfg.MODE_STAGE = EnapsoConfig.MODE_STAGE;
-encfg.MODE_DEFAULT = EnapsoConfig.MODE_DEFAULT;
 
-// publish the global encfg instance
-global.encfg = encfg;
+function getAllProperties(obj) {
+    var props = Object.keys(obj);
+    do {
+        props = props.concat(Object.getOwnPropertyNames(obj));
+    } while ((obj = Object.getPrototypeOf(obj)));
+
+    return props.sort().filter(function (item, idx, arr) {
+        return (
+            item != arr[idx + 1] && !item.startsWith('__')
+            /* && typeof toCheck[e] == 'function' */
+        );
+    });
+}
+
+var keys = getAllProperties(config);
+for (const key of keys) {
+    encfg[key] = config[key];
+}
 
 // publish requireEx to global scope for convenience
 global.requireEx = function (pkgRef) {
